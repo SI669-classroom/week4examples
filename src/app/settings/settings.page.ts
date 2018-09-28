5dd34f640dccfa015940667d1b07aeeff5c81287
Newman <mwnewman@m-c02sp0syh03m.local>
2018-09-27 22:25:32 -0400

    added Storage examples

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
@@ -10,16 +11,30 @@ export class SettingsPage implements OnInit {
   firstName: string = "";
   lastName: string=""; 
 
-  constructor() { 
-    this.firstName = localStorage.getItem('firstName');
-    this.lastName = localStorage.getItem('lastName');
+  constructor(public storage: Storage) { 
+    // this.firstName = localStorage.getItem('firstName');
+    // this.lastName = localStorage.getItem('lastName');
+
+    this.storage.get("data").then((data) => {
+      this.firstName = data["firstName"];
+      this.lastName = data["lastName"];
+      console.log("got data " + data);
+    });
+
+
   }
 
   ngOnInit() {
   }
 
-  saveData() {
+  saveDataLocal() {
     localStorage.setItem('firstName', this.firstName);
     localStorage.setItem('lastName', this.lastName);
+  }
+
+  saveData() {
+    let data = { "firstName": this.firstName,
+                 "lastName": this.lastName };
+    this.storage.set("data", data);
   }
 }
